74bc43590810eb22003f6e7a4154468de1a412ca
Yevhen Malyi <malyje164@gmail.com>
2022-07-22 20:32:20 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,92 @@
+//Exercise 1
+
+/* let user = {
+    name: "John",
+    years: 30
+}
+var { name, years } = user;
+console.log(name, years); */
+
+//Exercise 2
+
+/* let planet = ('Earth');
+let userName = ('John');
+console.log(planet) */
+
+
+//Exercise 3
+
+/* let phrase = "Hello"
+let user = "John";
+
+function sayHi() {
+    alert(`${phrase}, ${user}`)
+}
+sayHi() */
+
+//--------------------------
+//meaningless code
+//--------------------------
+
+/* let phrase = "Hello"
+
+if ("123") {
+    let user = "John";
+
+    function sayHi() {
+        alert(`${phrase}, ${user}`)
+    }
+}
+
+sayHi() */
+
+//Exercise 4
+
+/* var user = {
+    name: "John",
+    surname: "Smith"
+}
+user.name = "Pete";
+console.log(user)
+delete user.name
+console.log(user) */
+
+//Exercise 5
+
+/* const user = {
+    name: "John"
+}
+
+// does it work?
+user.name = "Pete"
+console.log(user) */
+
+//work
+
+
+//Exercise 6
+
+
+/* function salaries(obj) {
+    let sum = 0;
+    for (const salary of Object.values(obj)) {
+        if (salaries({})) {
+            return 0;
+        }
+        sum += salary;
+    }
+    return sum;
+}
+var obj = { Fred: 100, Ted: 160, Ghaith: 130 }
+
+console.log(salaries(obj)); */
+
+//Exercise 7
+
+
+/* (a + b < 4) ? "Below" : "Over"; */
+
+//Exercise 8
+
+/* 
+let message = (login == 'Employee') ? 'Hello' : (login == 'Director') ? 'Greetings' : (login == '') ? 'No login' : ''; */
